9b217d2c255ce9c9dbf0cc4ce0d737b6af34e54a
Jeiss <davidhedrin123@gmail.com>
2023-06-29 17:14:22 +0700

    Edit home product

--- app/Http/Livewire/HomeComponent.php
+++ app/Http/Livewire/HomeComponent.php
@@ -3,11 +3,33 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use App\Http\Halpers\HalperFunctions;
+use Illuminate\Support\Facades\Auth;
+use Exception;
+
+use App\Models\Product;
+use App\Models\ImageProduct;
+use App\Models\Category;
+use Livewire\WithPagination;
 
 class HomeComponent extends Component
 {
+    use WithPagination;
+
+    protected $paginationTheme = 'bootstrap';
+
+    public function loadAllData() {
+        $allProductImport = Product::where('product_for', 'i')->paginate();
+        $allProductExport = Product::where('product_for', 'e')->paginate(8);
+
+        return [
+            "allProductImport" => $allProductImport,
+            "allProductExport" => $allProductExport,
+        ];
+    }
+
     public function render()
     {
-        return view('livewire.home-component')->layout('layouts.base');
+        return view('livewire.home-component', $this->loadAllData())->layout('layouts.base');
     }
 }
--- resources/views/livewire/home-component.blade.php
+++ resources/views/livewire/home-component.blade.php
@@ -15,7 +15,7 @@
     </div>
   @endif
   
-  <div class="hero__item set-bg" data-setbg="{{ asset('assets/img/hero/banner.jpg') }}">
+  <div class="hero__item set-bg" data-setbg="{{ asset('banner/banner.png') }}">
     <div class="hero__text">
       <span>FRUIT FRESH</span>
       <h2>Vegetable <br />100% Organic</h2>
@@ -24,176 +24,72 @@
     </div>
   </div>
   
-  {{-- Slider product --}}
-  <div class="row mt-5">
-    <div class="categories__slider owl-carousel">
-      <div class="col-lg-3">
-        <div class="categories__item set-bg" data-setbg="{{ asset('assets/img/categories/cat-1.jpg') }}">
-          <h5><a href="#">Fresh Fruit</a></h5>
-        </div>
-      </div>
-      <div class="col-lg-3">
-        <div class="categories__item set-bg" data-setbg="{{ asset('assets/img/categories/cat-2.jpg') }}">
-          <h5><a href="#">Dried Fruit</a></h5>
-        </div>
-      </div>
-      <div class="col-lg-3">
-        <div class="categories__item set-bg" data-setbg="{{ asset('assets/img/categories/cat-3.jpg') }}">
-          <h5><a href="#">Vegetables</a></h5>
-        </div>
-      </div>
-      <div class="col-lg-3">
-        <div class="categories__item set-bg" data-setbg="{{ asset('assets/img/categories/cat-4.jpg') }}">
-          <h5><a href="#">drink fruits</a></h5>
-        </div>
-      </div>
-      <div class="col-lg-3">
-        <div class="categories__item set-bg" data-setbg="{{ asset('assets/img/categories/cat-5.jpg') }}">
-          <h5><a href="#">drink fruits</a></h5>
-        </div>
-      </div>
-    </div>
-  </div>
-
-  {{-- Future Product --}}
+  {{-- Import section --}}
   <div class="row mt-5">
     <div class="col-lg-12">
-      <div class="section-title">
-        <h2>Featured Product</h2>
-      </div>
-      <div class="featured__controls">
-        <ul>
-          <li class="active" data-filter="*">All</li>
-          <li data-filter=".oranges">Oranges</li>
-          <li data-filter=".fresh-meat">Fresh Meat</li>
-          <li data-filter=".vegetables">Vegetables</li>
-          <li data-filter=".fastfood">Fastfood</li>
-        </ul>
+      <div class="section-title from-blog__title">
+        <h2>Featured Import</h2>
       </div>
     </div>
   </div>
-  <div class="row featured__filter">
-    <div class="col-lg-3 col-md-4 col-sm-6 mix oranges fresh-meat">
-      <div class="featured__item">
-        <div class="featured__item__pic set-bg" data-setbg="{{ asset('assets/img/featured/feature-1.jpg') }}">
-          <ul class="featured__item__pic__hover">
-            <li><a href="#"><i class="fa fa-heart"></i></a></li>
-            <li><a href="#"><i class="fa fa-retweet"></i></a></li>
-            <li><a href="#"><i class="fa fa-shopping-cart"></i></a></li>
-          </ul>
+  {{-- Slider product --}}
+  @if (!empty($allProductImport))
+    <div class="row pb-4">
+      <div class="categories__slider owl-carousel">
+        @foreach ($allProductImport as $product)
+          <div class="col-lg-3">
+            <div class="categories__item set-bg" data-setbg="{{ asset('storage/'. colName('pr') . $product->image->image) }}">
+              <h5><a href="#">{{ $product->name }}</a></h5>
+            </div>
+          </div>
+        @endforeach
+      </div>
+    </div>
+    <div class="text-center pb-2">
+      <a href="#" class="primary-btn">MORE</a>
+    </div>
+  @endif
+
+  @if (!empty($allProductExport))
+    {{-- Future Product --}}
+    <div class="row mt-5">
+      <div class="col-lg-12">
+        <div class="section-title">
+          <h2>Featured Export</h2>
         </div>
-        <div class="featured__item__text">
-          <h6><a href="#">Crab Pool Security</a></h6>
-          <h5>$30.00</h5>
+        <div class="featured__controls">
+          <ul>
+            <li class="active" data-filter="*">All</li>
+            <li data-filter=".oranges">Oranges</li>
+            <li data-filter=".fresh-meat">Fresh Meat</li>
+            <li data-filter=".vegetables">Vegetables</li>
+            <li data-filter=".fastfood">Fastfood</li>
+          </ul>
         </div>
       </div>
     </div>
-    <div class="col-lg-3 col-md-4 col-sm-6 mix vegetables fastfood">
-      <div class="featured__item">
-        <div class="featured__item__pic set-bg" data-setbg="{{ asset('assets/img/featured/feature-2.jpg') }}">
-          <ul class="featured__item__pic__hover">
-            <li><a href="#"><i class="fa fa-heart"></i></a></li>
-            <li><a href="#"><i class="fa fa-retweet"></i></a></li>
-            <li><a href="#"><i class="fa fa-shopping-cart"></i></a></li>
-          </ul>
+    <div class="row featured__filter" style="justify-content: center">
+      @foreach ($allProductExport as $product)
+        <div class="col-lg-3 col-md-4 col-sm-6 mix oranges fresh-meat">
+          <div class="featured__item">
+            <div wire:click="" style="cursor: pointer" class="featured__item__pic set-bg" data-setbg="{{ asset('storage/'. colName('pr') . $product->image->image) }}">
+              <ul class="featured__item__pic__hover">
+                <li><a href="#"><i class="fa fa-heart"></i></a></li>
+                <li><a href="#"><i class="fa fa-shopping-cart"></i></a></li>
+              </ul>
+            </div>
+            <div class="featured__item__text">
+              <h6><a href="#">{{ $product->name }}</a></h6>
+              <h5>{{ currency_IDR($product->regular_price) }}</h5>
+            </div>
+          </div>
         </div>
-        <div class="featured__item__text">
-          <h6><a href="#">Crab Pool Security</a></h6>
-          <h5>$30.00</h5>
-        </div>
-      </div>
+      @endforeach
     </div>
-    <div class="col-lg-3 col-md-4 col-sm-6 mix vegetables fresh-meat">
-      <div class="featured__item">
-        <div class="featured__item__pic set-bg" data-setbg="{{ asset('assets/img/featured/feature-3.jpg') }}">
-          <ul class="featured__item__pic__hover">
-            <li><a href="#"><i class="fa fa-heart"></i></a></li>
-            <li><a href="#"><i class="fa fa-retweet"></i></a></li>
-            <li><a href="#"><i class="fa fa-shopping-cart"></i></a></li>
-          </ul>
-        </div>
-        <div class="featured__item__text">
-          <h6><a href="#">Crab Pool Security</a></h6>
-          <h5>$30.00</h5>
-        </div>
-      </div>
+    <div style="display: grid; justify-content: center">
+      {{ $allProductExport->links() }}
     </div>
-    <div class="col-lg-3 col-md-4 col-sm-6 mix fastfood oranges">
-      <div class="featured__item">
-        <div class="featured__item__pic set-bg" data-setbg="{{ asset('assets/img/featured/feature-4.jpg') }}">
-          <ul class="featured__item__pic__hover">
-            <li><a href="#"><i class="fa fa-heart"></i></a></li>
-            <li><a href="#"><i class="fa fa-retweet"></i></a></li>
-            <li><a href="#"><i class="fa fa-shopping-cart"></i></a></li>
-          </ul>
-        </div>
-        <div class="featured__item__text">
-          <h6><a href="#">Crab Pool Security</a></h6>
-          <h5>$30.00</h5>
-        </div>
-      </div>
-    </div>
-    <div class="col-lg-3 col-md-4 col-sm-6 mix fresh-meat vegetables">
-      <div class="featured__item">
-        <div class="featured__item__pic set-bg" data-setbg="{{ asset('assets/img/featured/feature-5.jpg') }}">
-          <ul class="featured__item__pic__hover">
-            <li><a href="#"><i class="fa fa-heart"></i></a></li>
-            <li><a href="#"><i class="fa fa-retweet"></i></a></li>
-            <li><a href="#"><i class="fa fa-shopping-cart"></i></a></li>
-          </ul>
-        </div>
-        <div class="featured__item__text">
-          <h6><a href="#">Crab Pool Security</a></h6>
-          <h5>$30.00</h5>
-        </div>
-      </div>
-    </div>
-    <div class="col-lg-3 col-md-4 col-sm-6 mix oranges fastfood">
-      <div class="featured__item">
-        <div class="featured__item__pic set-bg" data-setbg="{{ asset('assets/img/featured/feature-6.jpg') }}">
-          <ul class="featured__item__pic__hover">
-            <li><a href="#"><i class="fa fa-heart"></i></a></li>
-            <li><a href="#"><i class="fa fa-retweet"></i></a></li>
-            <li><a href="#"><i class="fa fa-shopping-cart"></i></a></li>
-          </ul>
-        </div>
-        <div class="featured__item__text">
-          <h6><a href="#">Crab Pool Security</a></h6>
-          <h5>$30.00</h5>
-        </div>
-      </div>
-    </div>
-    <div class="col-lg-3 col-md-4 col-sm-6 mix fresh-meat vegetables">
-      <div class="featured__item">
-        <div class="featured__item__pic set-bg" data-setbg="{{ asset('assets/img/featured/feature-7.jpg') }}">
-          <ul class="featured__item__pic__hover">
-            <li><a href="#"><i class="fa fa-heart"></i></a></li>
-            <li><a href="#"><i class="fa fa-retweet"></i></a></li>
-            <li><a href="#"><i class="fa fa-shopping-cart"></i></a></li>
-          </ul>
-        </div>
-        <div class="featured__item__text">
-          <h6><a href="#">Crab Pool Security</a></h6>
-          <h5>$30.00</h5>
-        </div>
-      </div>
-    </div>
-    <div class="col-lg-3 col-md-4 col-sm-6 mix fastfood vegetables">
-      <div class="featured__item">
-        <div class="featured__item__pic set-bg" data-setbg="{{ asset('assets/img/featured/feature-8.jpg') }}">
-          <ul class="featured__item__pic__hover">
-            <li><a href="#"><i class="fa fa-heart"></i></a></li>
-            <li><a href="#"><i class="fa fa-retweet"></i></a></li>
-            <li><a href="#"><i class="fa fa-shopping-cart"></i></a></li>
-          </ul>
-        </div>
-        <div class="featured__item__text">
-          <h6><a href="#">Crab Pool Security</a></h6>
-          <h5>$30.00</h5>
-        </div>
-      </div>
-    </div>
-  </div>
+  @endif
 
   {{-- Two Banner --}}
   <div class="row mt-5">
@@ -416,49 +312,82 @@
       </div>
     </div>
   </div>
-  <div class="row">
-    <div class="col-lg-4 col-md-4 col-sm-6">
-      <div class="blog__item">
-        <div class="blog__item__pic">
-          <img src="{{ asset('assets/img/blog/blog-1.jpg') }}" alt="">
-        </div>
-        <div class="blog__item__text">
-          <ul>
-            <li><i class="fa fa-calendar-o"></i> May 4,2019</li>
-            <li><i class="fa fa-comment-o"></i> 5</li>
-          </ul>
-          <h5><a href="#">Cooking tips make cooking simple</a></h5>
-          <p>Sed quia non numquam modi tempora indunt ut labore et dolore magnam aliquam quaerat </p>
-        </div>
-      </div>
-    </div>
-    <div class="col-lg-4 col-md-4 col-sm-6">
-      <div class="blog__item">
-        <div class="blog__item__pic">
-          <img src="{{ asset('assets/img/blog/blog-2.jpg') }}" alt="">
-        </div>
-        <div class="blog__item__text">
-          <ul>
-            <li><i class="fa fa-calendar-o"></i> May 4,2019</li>
-            <li><i class="fa fa-comment-o"></i> 5</li>
-          </ul>
-          <h5><a href="#">6 ways to prepare breakfast for 30</a></h5>
-          <p>Sed quia non numquam modi tempora indunt ut labore et dolore magnam aliquam quaerat </p>
+  
+  <div class="row mt-3">
+    <div class="categories__slider owl-carousel">
+      <div class="col-lg-3">
+        <div class="blog__item">
+          <div class="blog__item__pic">
+            <img src="{{ asset('assets/img/blog/blog-1.jpg') }}" alt="">
+          </div>
+          <div class="blog__item__text">
+            <ul>
+              <li><i class="fa fa-calendar-o"></i> May 4,2019</li>
+              <li><i class="fa fa-comment-o"></i> 5</li>
+            </ul>
+            <h5><a href="#">Cooking tips make cooking simple</a></h5>
+            <p>Sed quia non numquam modi tempora indunt ut labore et dolore magnam aliquam quaerat </p>
+          </div>
         </div>
       </div>
-    </div>
-    <div class="col-lg-4 col-md-4 col-sm-6">
-      <div class="blog__item">
-        <div class="blog__item__pic">
-          <img src="{{ asset('assets/img/blog/blog-3.jpg') }}" alt="">
+      <div class="col-lg-3">
+        <div class="blog__item">
+          <div class="blog__item__pic">
+            <img src="{{ asset('assets/img/blog/blog-2.jpg') }}" alt="">
+          </div>
+          <div class="blog__item__text">
+            <ul>
+              <li><i class="fa fa-calendar-o"></i> May 4,2019</li>
+              <li><i class="fa fa-comment-o"></i> 5</li>
+            </ul>
+            <h5><a href="#">6 ways to prepare breakfast for 30</a></h5>
+            <p>Sed quia non numquam modi tempora indunt ut labore et dolore magnam aliquam quaerat </p>
+          </div>
         </div>
-        <div class="blog__item__text">
-          <ul>
-            <li><i class="fa fa-calendar-o"></i> May 4,2019</li>
-            <li><i class="fa fa-comment-o"></i> 5</li>
-          </ul>
-          <h5><a href="#">Visit the clean farm in the US</a></h5>
-          <p>Sed quia non numquam modi tempora indunt ut labore et dolore magnam aliquam quaerat </p>
+      </div>
+      <div class="col-lg-3">
+        <div class="blog__item">
+          <div class="blog__item__pic">
+            <img src="{{ asset('assets/img/blog/blog-3.jpg') }}" alt="">
+          </div>
+          <div class="blog__item__text">
+            <ul>
+              <li><i class="fa fa-calendar-o"></i> May 4,2019</li>
+              <li><i class="fa fa-comment-o"></i> 5</li>
+            </ul>
+            <h5><a href="#">Visit the clean farm in the US</a></h5>
+            <p>Sed quia non numquam modi tempora indunt ut labore et dolore magnam aliquam quaerat </p>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-3">
+        <div class="blog__item">
+          <div class="blog__item__pic">
+            <img src="{{ asset('assets/img/blog/blog-3.jpg') }}" alt="">
+          </div>
+          <div class="blog__item__text">
+            <ul>
+              <li><i class="fa fa-calendar-o"></i> May 4,2019</li>
+              <li><i class="fa fa-comment-o"></i> 5</li>
+            </ul>
+            <h5><a href="#">Visit the clean farm in the US</a></h5>
+            <p>Sed quia non numquam modi tempora indunt ut labore et dolore magnam aliquam quaerat </p>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-3">
+        <div class="blog__item">
+          <div class="blog__item__pic">
+            <img src="{{ asset('assets/img/blog/blog-2.jpg') }}" alt="">
+          </div>
+          <div class="blog__item__text">
+            <ul>
+              <li><i class="fa fa-calendar-o"></i> May 4,2019</li>
+              <li><i class="fa fa-comment-o"></i> 5</li>
+            </ul>
+            <h5><a href="#">6 ways to prepare breakfast for 30</a></h5>
+            <p>Sed quia non numquam modi tempora indunt ut labore et dolore magnam aliquam quaerat </p>
+          </div>
         </div>
       </div>
     </div>
